332261804a61d3f8f11666de8842081e7eb9f83c
Yazle Ligot <60304977@udst.edu.qa>
2026-02-11 20:18:35 +0300

    fix #1: delete words

--- script.js
+++ script.js
@@ -149,10 +149,10 @@ function editWord(index) {
 
 
 function deleteWord(index) {
-    if (!confirm('Are you sure you want to delete this word?')) {
+    if (!confirm('Are you sure you want to delete this word?')) return;
+        wordBank = wordBank.splice(index, 1);
         saveWordBank();
         displayWordBank();
-    }
 }
 
 function generateKeyboard() {
